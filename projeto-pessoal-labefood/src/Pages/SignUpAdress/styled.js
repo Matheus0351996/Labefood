eb11a9a179a081bb59acd0e2e37b5129963d6c21
matheus <mnatalsantos@hotmail.com>
2022-11-20 15:11:43 -0300

    style(signUp/ signUpAdress): fim up/start-Adress

---
+++ projeto-pessoal-labefood/src/Pages/SignUpAdress/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+padding:10px;
+width:100%;
+height:100vh;
+display:flex;
+flex-direction:column;
+align-items:center;
+
+form{
+    margin-top: 50px;
+    display:flex;
+    width: 90%;
+    height: 50%;
+    justify-content:space-evenly;
+    flex-direction:column;
+}
+
+p{
+    font-size:2rem;
+}
+`
